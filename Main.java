ff4a3d4d2201d1ac3bf96959ef85e3838c235bf6
GiancarloAlarconCubas <N00452114@upn.pe>
2026-08-30 23:32:46 -0500

    feat: agregar nuevo estudiante

--- Main.java
+++ Main.java
@@ -4,10 +4,16 @@ public class Main {
         Estudiante e1 = new Estudiante("Ana García", 1001, 16.5);
         Estudiante e2 = new Estudiante("Luis Torres", 1002, 14.0);
         Estudiante e3 = new Estudiante("Rosa Díaz",  1003, 18.0);
+        Estudiante e7 = new Estudiante("Ana Robles", 1007, 16.5);
+        Estudiante e8 = new Estudiante("Paola García", 1008, 15.5);
+        Estudiante e9 = new Estudiante("Juan Perez", 1009, 17.5);
  
         e1.mostrarInfo();
         e2.mostrarInfo();
         e3.mostrarInfo();
+        e7.mostrarInfo();
+        e8.mostrarInfo();
+        e9.mostrarInfo();
  
         System.out.println("Total estudiantes: " + Estudiante.getTotalEstudiantes());
     }
